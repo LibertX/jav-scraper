01f93f552f8b3e96ec4c57889bd446e673a4ad40
LibertX <libertx@ltsp.fr>
2023-09-23 11:23:30 +0200

    Improve DB to avoid null fields

--- jav_scraper/models/javmovie.py
+++ jav_scraper/models/javmovie.py
@@ -5,8 +5,8 @@ class JAVMovie(db.Model):
     __tablename__ = "jav_movie"
 
     id: Mapped[int] = db.Column(db.Integer, primary_key=True)
-    code: Mapped[str] = db.Column(db.String)
-    status = db.Column(db.String)
+    code: Mapped[str] = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False)
     grabs = db.relationship('Grab', backref='jav_movie', cascade="all, delete-orphan", lazy=True)
 
     quality_id = db.Column(db.Integer, db.ForeignKey("jav_quality.id"), nullable=True)
--- jav_scraper/models/javquality.py
+++ jav_scraper/models/javquality.py
@@ -4,4 +4,4 @@ class JAVQuality(db.Model):
     __tablename__ = "jav_quality"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
